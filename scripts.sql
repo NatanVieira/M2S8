d0da2efa20db889c67782d08ec9ec0adb760b279
Natanael Vieira <natanaelvieira130@gmail.com>
2022-07-19 13:13:12 -0300

    M2S8 - Exercicio 06

--- scripts.sql
+++ scripts.sql
@@ -1,23 +1,62 @@
-/*create database empresa;*/
+-- Exercicio 5 - Criando banco e tabelas
+--create database empresa;
+
+--use Empresa;
+
+--create table Empregados 
+--(
+--	id_emp int primary key identity(1,1) not null,
+--	nome_emp varchar(50) not null,
+--	endereco varchar(100) not null,
+--	idade int not null
+--);
+
+--create table Cargos 
+--(
+--	id_cargo int primary key identity(1,1) not null,
+--	nome_cargo varchar(50) not null,
+--);
+
+--create table Gestores 
+--(
+--	id_gestor int primary key identity(1,1) not null,
+--	nome_gestor varchar(50) not null,
+--);
+
+
+-- Exercicio 06 - Populando tabelas
 
 use Empresa;
 
-create table Empregados 
-(
-	id_emp int primary key identity(1,1) not null,
-	nome_emp varchar(50) not null,
-	endereco varchar(100) not null,
-	idade int not null
-);
+insert into Empregados (nome_emp, endereco, idade) values ('Sirius Black', 'Rua Teste 2', 52),
+													      ('James Potter', 'Rua Teste 3', 50),
+														  ('Ronie Wesley', 'Rua Teste 4', 20),
+														  ('Hermione Granger', 'Rua Teste 5', 20),
+														  ('Severus Snape', 'Rua Teste 6', 53),
+														  ('Nicolau Flamel', 'Rua Teste 7', 600),
+														  ('Fredie Wesley', 'Rua Teste 8', 23),
+														  ('Harry Potter', 'Rua Teste 9', 20),
+														  ('Gina Wesley', 'Rua Teste 10', 18),
+														  ('Lord Valdemort', 'Rua Teste 11', 70);
 
-create table Cargos 
-(
-	id_cargo int primary key identity(1,1) not null,
-	nome_cargo varchar(50) not null,
-);
+insert into Cargos (nome_cargo) values ('Professor'),
+									   ('Diretor'),
+									   ('Comensal da Morte'),
+									   ('Pesquisador'),
+									   ('Vendedor'),
+									   ('Auror'),
+									   ('Jogador de Quadribol'),
+									   ('Treinador'),
+									   ('Guarda'),
+									   ('Inspetor');	
 
-create table Gestores 
-(
-	id_gestor int primary key identity(1,1) not null,
-	nome_gestor varchar(50) not null,
-);
+insert into Gestores (nome_gestor) values ('Alvo Dumbledore'),
+									     ('Belatrix Lestrange'),
+										 ('Luna Lovegood'),
+										 ('Dobby'),
+										 ('Remo Lupin'),
+										 ('Minerva McGonagall'),
+										 ('Ninfadora Tonks'),
+										 ('Alastor Moody'),
+										 ('Molly Weasley'),
+										 ('Newt Scamander');
