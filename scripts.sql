b481abb60b04b018f5ae6994c7d65a20e5944c07
Natanael Vieira <natanaelvieira130@gmail.com>
2022-07-19 13:17:32 -0300

    M2S08 - Exercicio 07

--- scripts.sql
+++ scripts.sql
@@ -26,37 +26,42 @@
 
 -- Exercicio 06 - Populando tabelas
 
+--use Empresa;
+
+--insert into Empregados (nome_emp, endereco, idade) values ('Sirius Black', 'Rua Teste 2', 52),
+--													      ('James Potter', 'Rua Teste 3', 50),
+--														  ('Ronie Wesley', 'Rua Teste 4', 20),
+--														  ('Hermione Granger', 'Rua Teste 5', 20),
+--														  ('Severus Snape', 'Rua Teste 6', 53),
+--														  ('Nicolau Flamel', 'Rua Teste 7', 600),
+--														  ('Fredie Wesley', 'Rua Teste 8', 23),
+--														  ('Harry Potter', 'Rua Teste 9', 20),
+--														  ('Gina Wesley', 'Rua Teste 10', 18),
+--														  ('Lord Valdemort', 'Rua Teste 11', 70);
+
+--insert into Cargos (nome_cargo) values ('Professor'),
+--									   ('Diretor'),
+--									   ('Comensal da Morte'),
+--									   ('Pesquisador'),
+--									   ('Vendedor'),
+--									   ('Auror'),
+--									   ('Jogador de Quadribol'),
+--									   ('Treinador'),
+--									   ('Guarda'),
+--									   ('Inspetor');	
+
+--insert into Gestores (nome_gestor) values ('Alvo Dumbledore'),
+--									     ('Belatrix Lestrange'),
+--										 ('Luna Lovegood'),
+--										 ('Dobby'),
+--										 ('Remo Lupin'),
+--										 ('Minerva McGonagall'),
+--										 ('Ninfadora Tonks'),
+--										 ('Alastor Moody'),
+--										 ('Molly Weasley'),
+--										 ('Newt Scamander');
+
+--Exercicio 7
 use Empresa;
 
-insert into Empregados (nome_emp, endereco, idade) values ('Sirius Black', 'Rua Teste 2', 52),
-													      ('James Potter', 'Rua Teste 3', 50),
-														  ('Ronie Wesley', 'Rua Teste 4', 20),
-														  ('Hermione Granger', 'Rua Teste 5', 20),
-														  ('Severus Snape', 'Rua Teste 6', 53),
-														  ('Nicolau Flamel', 'Rua Teste 7', 600),
-														  ('Fredie Wesley', 'Rua Teste 8', 23),
-														  ('Harry Potter', 'Rua Teste 9', 20),
-														  ('Gina Wesley', 'Rua Teste 10', 18),
-														  ('Lord Valdemort', 'Rua Teste 11', 70);
-
-insert into Cargos (nome_cargo) values ('Professor'),
-									   ('Diretor'),
-									   ('Comensal da Morte'),
-									   ('Pesquisador'),
-									   ('Vendedor'),
-									   ('Auror'),
-									   ('Jogador de Quadribol'),
-									   ('Treinador'),
-									   ('Guarda'),
-									   ('Inspetor');	
-
-insert into Gestores (nome_gestor) values ('Alvo Dumbledore'),
-									     ('Belatrix Lestrange'),
-										 ('Luna Lovegood'),
-										 ('Dobby'),
-										 ('Remo Lupin'),
-										 ('Minerva McGonagall'),
-										 ('Ninfadora Tonks'),
-										 ('Alastor Moody'),
-										 ('Molly Weasley'),
-										 ('Newt Scamander');
+alter table Empregados add setor varchar(30) not null default 'engenharia';
